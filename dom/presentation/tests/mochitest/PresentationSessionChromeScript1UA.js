50de634a4f934b0f5897a4d1309e7ef27bfdd39d
KuoE0 <kuoe0.tw@gmail.com>
2016-06-02 18:04:09 +0800

    Bug 1277494 - Use random session ID to fix intermittent crash for unit test of Presentation API on b2g v2.6; r=me, a=jocheng
    MozReview-Commit-ID: KnpIap4bEgx

--- dom/presentation/tests/mochitest/PresentationSessionChromeScript1UA.js
+++ dom/presentation/tests/mochitest/PresentationSessionChromeScript1UA.js
@@ -9,12 +9,15 @@ const { classes: Cc, interfaces: Ci, manager: Cm, utils: Cu, results: Cr } = Com
 
 Cu.import('resource://gre/modules/XPCOMUtils.jsm');
 
+const uuidGenerator = Cc["@mozilla.org/uuid-generator;1"]
+                      .getService(Ci.nsIUUIDGenerator);
+
 function debug(str) {
   // dump('DEBUG -*- PresentationSessionChromeScript1UA -*-: ' + str + '\n');
 }
 
 const originalFactoryData = [];
-const sessionId = 'test-session-id';
+const sessionId = 'test-session-id-' + uuidGenerator.generateUUID().toString();
 const address = Cc["@mozilla.org/supports-cstring;1"]
                   .createInstance(Ci.nsISupportsCString);
 address.data = "127.0.0.1";
